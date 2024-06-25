81606f372fccfa32301fbdbcc0e5f687a3ea3097
yyk-work <150160929+yyk-work@users.noreply.github.com>
2024-06-25 15:29:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import networkx as nx
+
 
 
 st.title("🕸 Internal Link Evaluation App")
@@ -20,36 +22,6 @@ if uploaded_file is not None:
 
 df = dataframe
 
-# classify rows
-def classify_links(source, destination, anchor, similarity, similiarity_cleaned):
-    match (source, destination, anchor, similarity, similiarity_cleaned):
-        #case _ if len(anchor) <= 5:
-            #return 'Short Anchor'
-        case _ if 'encoded' in destination:
-            return 'Encoded Link'
-        case _ if 'encoded' in source:
-            return 'Encoded Link'
-        case _ if 'v_' in destination:
-            return 'Filter Link'
-        case _ if 'google' in destination:
-            return 'Google Link'
-        case _ if 'pdf' in destination:
-            return 'PDF Link'
-        case _ if 'media' in destination:
-            return 'Media Link'
-        case _ if similiarity_cleaned < 20:
-            return 'No Match'
-        case _ if similarity > 98:
-            return 'Complete Match'
-        case _ if 20 <= similiarity_cleaned < 60:
-            return 'Bad Match'
-        case _ if similiarity_cleaned >= 60:
-            return 'Good Match'
-        case _ if source == destination:
-            return 'Anchor Link'
-        case _:
-            return 'Other'
-
 # clean dataframe
 df['Source'] = df['Source'].astype('string')
 df['Destination'] = df['Destination'].astype('string')
@@ -65,6 +37,8 @@ df['Status Code'].fillna('none', inplace=True)
 
 # Display the DataFrame
 st.write(df)
+
+
 
 st.write(
     "Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:"
