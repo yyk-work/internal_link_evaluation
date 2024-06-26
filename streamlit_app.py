f75e7ead158ee580ed0de9ff6932e99793f58f33
yyk-work <150160929+yyk-work@users.noreply.github.com>
2024-06-26 09:01:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,26 +16,18 @@ uploaded_file = st.file_uploader("Upload a CSV file with 'Source' and 'Destinati
 
 # If a file is uploaded
 if uploaded_file is not None:
-    # Read the CSV file into a DataFrame
     edges_df = pd.read_csv(uploaded_file)
-
-    # Create a directed graph (DiGraph)
     G = nx.DiGraph()
-
-    # Add edges from the DataFrame
     edges = [(row['Source'], row['Destination']) for _, row in edges_df.iterrows()]
     G.add_edges_from(edges)
-
-    # Calculate PageRank
     pagerank = nx.pagerank(G)
 
-    # Create a CSV file for download
-    # Create a CSV file for download
+    # Create a DataFrame from the pagerank dictionary
+    pagerank_df = pd.DataFrame(pagerank.items(), columns=['Node', 'Score'])
+
+    # Create csv
     csv_file_path = "pagerank_scores.csv"
-    with open(csv_file_path, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["Node", "Score"])  # Write header
-        writer.writerows(pagerank.items())  # Write data rows
+    pagerank_df.to_csv(csv_file_path)
 
     # Provide a download link
     st.download_button("Download CSV", csv_file_path, file_name="pagerank_scores.csv", key="download_button")
